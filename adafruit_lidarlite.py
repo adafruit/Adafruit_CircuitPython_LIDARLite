f3271084c4be40b917902545d59835998b4d3993
ladyada <limor@ladyada.net>
2018-09-29 13:27:09 -0400

    its..reset?

--- adafruit_lidarlite.py
+++ adafruit_lidarlite.py
@@ -86,9 +86,13 @@ class LIDARLite:
     """
 
     def __init__(self, i2c_bus, *, reset_pin=None, configuration=CONFIG_DEFAULT, address=_ADDR_DEFAULT):
+        self.i2c_bus = i2c_bus
         self.i2c_device = I2CDevice(i2c_bus, address)
         self._buf = bytearray(2)
-
+        time.sleep(0.5)
+        self.configure(configuration)
+    
+    def reset(self):
         # Optional hardware reset pin
         if reset_pin is not None:
             reset_pin.direction = Direction.OUTPUT
@@ -96,21 +100,25 @@ class LIDARLite:
             reset_pin.value = False
             time.sleep(0.01)
             reset_pin.value = True
-        self.reset()
-        self.configure(configuration)
-    
-    def reset(self):
-        try:
-            self._write_reg(_REG_ACQ_COMMAND, _CMD_RESET)
-        except OSError:
-            pass   # it doesnt respond well once reset
-        time.sleep(0.05)
+        else:
+            try:
+                self._write_reg(_REG_ACQ_COMMAND, _CMD_RESET)
+            except OSError:
+                pass   # it doesnt respond well once reset
+        time.sleep(3)
 
     def configure(self, config):
         settings = _configurations[config]
         self._write_reg(0x02, settings[0])
         self._write_reg(0x04, settings[1])
         self._write_reg(0x1c, settings[2])
+
+    def get_distance(self):
+
+        # write & wait
+        with self.i2c_device as i2c:
+            i2c.write(bytes([0, 4]))
+        time.sleep(0.02)  # in arduino library
 
     def read_distance(self, bias=False):
         if bias:
@@ -127,9 +135,15 @@ class LIDARLite:
 
     @property
     def distance(self):
-        return self.read_distance()  # try to read the distance
-
-
+        #return self.read_distance()  # try to read the distance
+        self._write_reg(0x00, 0x04)
+        buf = self._read_reg(0x8F, 2)
+        #with self.i2c_device as i2c:
+        #    buf = bytearray(2)
+        #    i2c.write(bytes([0x8F]))
+        #    i2c.readinto( buf)
+        return buf[0] << 8 | buf[1]
+  
     @property
     def bias_distance(self):
         return self.read_distance(True)
@@ -148,20 +162,19 @@ class LIDARLite:
         self._buf[0] = reg
         self._buf[1] = value
         with self.i2c_device as i2c:
-            print("Writing: ", [hex(i) for i in self._buf])
+            #print("Writing: ", [hex(i) for i in self._buf])
             i2c.write(self._buf)
         time.sleep(0.02)  # in arduino library
 
     def _read_reg(self, reg, len):
         while True:
             self._status = self.status
-            if self._status & 0x3F == STATUS_HEALTHY :
+            if not self._status & STATUS_BUSY:
                 break
-        
+        # no longer busy
         self._buf[0] = reg
-        inn = bytearray(2)
         with self.i2c_device as i2c:
-            i2c.write(bytes([reg]))
+            i2c.write(self._buf, end=1)
             i2c.readinto(self._buf, end=len)
-        print("Read from ", hex(reg), [hex(i) for i in self._buf])
+        #print("Read from ", hex(reg), [hex(i) for i in self._buf])
         return self._buf
