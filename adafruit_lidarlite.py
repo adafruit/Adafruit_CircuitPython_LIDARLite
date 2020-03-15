2eb6911cf2773ddf3e6457acf507e2d7078ab84f
dherrada <dylan.herrada@gmail.com>
2020-03-15 16:56:03 -0400

    Ran black, updated to pylint 2.x

--- adafruit_lidarlite.py
+++ adafruit_lidarlite.py
@@ -74,12 +74,15 @@ STATUS_HEALTHY = 0x20
 STATUS_SYS_ERROR = 0x40
 
 # The various configuration register values, from arduino library
-_LIDAR_CONFIGS = ((0x80, 0x08, 0x00), # default
-                  (0x1D, 0x08, 0x00), # short range, high speed
-                  (0x80, 0x00, 0x00), # default range, higher speed short range
-                  (0xFF, 0x08, 0x00), # maximum range
-                  (0x80, 0x08, 0x80), # high sensitivity & error
-                  (0x80, 0x08, 0xb0)) # low sensitivity & error
+_LIDAR_CONFIGS = (
+    (0x80, 0x08, 0x00),  # default
+    (0x1D, 0x08, 0x00),  # short range, high speed
+    (0x80, 0x00, 0x00),  # default range, higher speed short range
+    (0xFF, 0x08, 0x00),  # maximum range
+    (0x80, 0x08, 0x80),  # high sensitivity & error
+    (0x80, 0x08, 0xB0),
+)  # low sensitivity & error
+
 
 class LIDARLite:
     """
@@ -89,8 +92,14 @@ class LIDARLite:
     :param int address: (optional) The I2C address of the device to set after initialization.
     """
 
-    def __init__(self, i2c_bus, *, reset_pin=None,
-                 configuration=CONFIG_DEFAULT, address=_ADDR_DEFAULT):
+    def __init__(
+        self,
+        i2c_bus,
+        *,
+        reset_pin=None,
+        configuration=CONFIG_DEFAULT,
+        address=_ADDR_DEFAULT
+    ):
         """Initialize the hardware for the LIDAR over I2C. You can pass in an
         optional reset_pin for when you call reset(). There are a few common
         configurations Garmin suggests: CONFIG_DEFAULT, CONFIG_SHORTFAST,
@@ -121,7 +130,7 @@ class LIDARLite:
             try:
                 self._write_reg(_REG_ACQ_COMMAND, _CMD_RESET)
             except OSError:
-                pass   # it doesnt respond well once reset
+                pass  # it doesnt respond well once reset
         time.sleep(1)
         # take 100 readings to 'flush' out sensor!
         for _ in range(100):
@@ -138,7 +147,7 @@ class LIDARLite:
         settings = _LIDAR_CONFIGS[config]
         self._write_reg(0x02, settings[0])
         self._write_reg(0x04, settings[1])
-        self._write_reg(0x1c, settings[2])
+        self._write_reg(0x1C, settings[2])
 
     def read_distance(self, bias=False):
         """Perform a distance reading with or without 'bias'. It's recommended
@@ -159,7 +168,7 @@ class LIDARLite:
         """The measured distance in cm. Will take a bias reading every 100 calls"""
         self._bias_count -= 1
         if self._bias_count < 0:
-            self._bias_count = 100 # every 100 reads, check bias
+            self._bias_count = 100  # every 100 reads, check bias
         return self.read_distance(self._bias_count <= 0)
 
     @property
@@ -174,7 +183,7 @@ class LIDARLite:
         self._buf[0] = reg
         self._buf[1] = value
         with self.i2c_device as i2c:
-            #print("Writing: ", [hex(i) for i in self._buf])
+            # print("Writing: ", [hex(i) for i in self._buf])
             i2c.write(self._buf)
         time.sleep(0.001)  # there's a delay in arduino library
 
@@ -187,5 +196,5 @@ class LIDARLite:
         self._buf[0] = reg
         with self.i2c_device as i2c:
             i2c.write_then_readinto(self._buf, self._buf, out_end=1, in_end=num)
-        #print("Read from ", hex(reg), [hex(i) for i in self._buf])
+        # print("Read from ", hex(reg), [hex(i) for i in self._buf])
         return self._buf
