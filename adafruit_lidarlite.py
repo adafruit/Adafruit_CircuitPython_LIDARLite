cd4dfced972a7bcead772464bec45c2128004bb9
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:13 -0400

    Update version string

--- adafruit_lidarlite.py
+++ adafruit_lidarlite.py
@@ -32,7 +32,7 @@ from adafruit_bus_device.i2c_device import I2CDevice
 from digitalio import Direction
 from micropython import const
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_LIDARLite.git"
 
 
